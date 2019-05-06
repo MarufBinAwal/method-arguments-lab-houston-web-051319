e7f736fa9194ae53c3b2a229c2493166c1f432ba
MARUF B AWAL <marufbinawal@gmail.com>
2019-05-06 14:26:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,4 +2,5 @@ def introduction(name)
   puts "Hi my name is #{name}."
 end
 
-def introduction("Dan", "")
+def introduction("Dan", "Hi, my name is Dan and I am learning to program in Ember.js.")
+end
